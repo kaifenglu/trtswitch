926e10bab85687e8fc3fac62ef7681bbe0afadc7
Kaifeng Lu <kaifenglu@gmail.com>
2024-10-18 11:25:54 -0400

    add data

---
+++ data-raw/DATASET.R
@@ -0,0 +1,19 @@
+aml = lrstat::aml
+heart = lrstat::heart
+immdef = lrstat::immdef
+ingots = lrstat::ingots
+rawdata = lrstat::rawdata
+shilong = lrstat::shilong
+six = lrstat::six
+tobin = lrstat::tobin
+sexagg = logistf::sexagg
+
+usethis::use_data(aml, overwrite = TRUE)
+usethis::use_data(heart, overwrite = TRUE)
+usethis::use_data(immdef, overwrite = TRUE)
+usethis::use_data(ingots, overwrite = TRUE)
+usethis::use_data(rawdata, overwrite = TRUE)
+usethis::use_data(shilong, overwrite = TRUE)
+usethis::use_data(six, overwrite = TRUE)
+usethis::use_data(tobin, overwrite = TRUE)
+usethis::use_data(sexagg, overwrite = TRUE)
