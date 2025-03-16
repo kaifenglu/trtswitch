db463092d475b1f719fc04a1d68d4e8b6c30a15a
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:25:43 -0400

    add testthat:: prefix

--- tests/testthat/test-kmest.R
+++ tests/testthat/test-kmest.R
@@ -1,27 +1,27 @@
 library(survival)
 
-test_that("kmest: estimate and standard error", {
+testthat::test_that("kmest: estimate and standard error", {
   df1 <- kmest(aml, stratum="x", time="time", event="status",
                conftype="none")
-  
-  df2 <- summary(survfit(Surv(time, status) ~ x, data=aml, 
+
+  df2 <- summary(survfit(Surv(time, status) ~ x, data=aml,
                          conf.type="none"))
-  
-  expect_equal(df1$survival, df2$surv)
-  expect_equal(df1$stderr, df2$std.err)
+
+  testthat::expect_equal(df1$survival[df1$time > 0], df2$surv)
+  testthat::expect_equal(df1$stderr[df1$time > 0], df2$std.err)
 })
 
 
-test_that("kmest: confidence interval", {
+testthat::test_that("kmest: confidence interval", {
   for (conftype in c("plain", "log", "log-log", "arcsin")) {
     df1 <- kmest(aml, stratum="x", time="time", event="status",
                  conftype=conftype)
-    
-    df2 <- summary(survfit(Surv(time, status) ~ x, data=aml, 
+
+    df2 <- summary(survfit(Surv(time, status) ~ x, data=aml,
                            conf.type=conftype))
-    
-    expect_equal(df1$lower, df2$lower)
-    expect_equal(df1$upper, df2$upper)  
+
+    testthat::expect_equal(df1$lower[df1$time > 0], df2$lower)
+    testthat::expect_equal(df1$upper[df1$time > 0], df2$upper)
   }
 })
 
