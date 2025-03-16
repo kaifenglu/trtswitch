bfed9742ca2724a1d7d1db9b66452c25bcc5db05
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:47:17 -0400

    add testthat:: prefix

--- tests/testthat/test-splineDesigncpp.R
+++ tests/testthat/test-splineDesigncpp.R
@@ -1,9 +1,13 @@
-test_that("splineDesigncpp: default values", {
-  expect_equal(splineDesigncpp(knots = 1:10, x = 4:7),
-               splines::splineDesign(knots = 1:10, x = 4:7))
+library(splines)
+
+testthat::test_that("splineDesigncpp: default values", {
+  testthat::expect_equal(splineDesigncpp(knots = 1:10, x = 4:7),
+                         splineDesign(knots = 1:10, x = 4:7))
 })
 
-test_that("splineDesigncpp: derivatives", {
-  expect_equal(splineDesigncpp(knots = 1:10, x = 4:7, derivs = 0:2),
-               splines::splineDesign(knots = 1:10, x = 4:7, derivs = 0:2))
+testthat::test_that("splineDesigncpp: derivatives", {
+  testthat::expect_equal(splineDesigncpp(knots = 1:10, x = 4:7, 
+                                         derivs = 0:2),
+                         splineDesign(knots = 1:10, x = 4:7, 
+                                      derivs = 0:2))
 })
