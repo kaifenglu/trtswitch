95f8f95631bcc4f34c318aaadbd1c15a74db3ce8
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:25:20 -0400

    - replace survdiff with lrtest - replace survreg with liferegr - replace coxph with phregr

--- tests/testthat/test-ipe.R
+++ tests/testthat/test-ipe.R
@@ -1,5 +1,4 @@
 library(dplyr, warn.conflicts = FALSE)
-library(survival)
 
 testthat::test_that("ipe: control to active switch", {
   data1 <- immdef %>% mutate(rx = 1-xoyrs/progyrs)
@@ -10,8 +9,8 @@ testthat::test_that("ipe: control to active switch", {
     boot = FALSE)
   
   # log-rank for ITT
-  fit_lr <- survdiff(Surv(progyrs, prog) ~ imm, data = data1)
-  z_lr = (fit_lr$obs - fit_lr$exp)[2]/sqrt(fit_lr$var[2,2])
+  fit_lr <- lrtest(data1, treat = "imm", time = "progyrs", event = "prog")
+  z_lr = fit_lr$logRankZ
   
   f <- function(psi) {
     data1 %>%
@@ -29,17 +28,17 @@ testthat::test_that("ipe: control to active switch", {
   
   g <- function(psi) {
     data2 <- f(psi)
-    fit_aft <- survreg(Surv(t_star, d_star) ~ imm, 
-                       data = data2, dist = "weibull")
-    -fit_aft$coefficients[2] - psi
+    fit_aft <- liferegr(data2, time = "t_star", event = "d_star", 
+                        covariates = "imm", dist = "weibull")
+    -fit_aft$beta[2] - psi
   }
   
   # psi based on AFT model
   psi <- uniroot(g, c(-3,3), tol = 1e-6)$root
   
   data2 <- f(psi)
-  fit <- coxph(Surv(t_star, d_star) ~ imm, data = data2)
-  beta = as.numeric(fit$coefficients[1])
+  fit <- phregr(data2, time = "t_star", event = "d_star", covariates = "imm")
+  beta = as.numeric(fit$beta[1])
   se = beta/z_lr
   
   zcrit = qnorm(0.975)
