253a89856b76ea40526a4ce17879a8bf126ce364
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:11:33 -0400

    - add link to struct logparams - add f_firth_score_0 - add the bc parameter to logisregloop and logisregcpp

--- src/logistic_regression.h
+++ src/logistic_regression.h
@@ -8,6 +8,7 @@ using namespace Rcpp;
 
 struct logparams {
   int n;
+  std::string link;
   NumericVector y;
   NumericMatrix z;
   NumericVector freq;
@@ -15,25 +16,22 @@ struct logparams {
   NumericVector offset;
 };
 
-
 double f_llik_0(int p, NumericVector par, void *ex);
-
 NumericVector f_score_0(int p, NumericVector par, void *ex);
-
 NumericMatrix f_info_0(int p, NumericVector par, void *ex);
 
 double f_pen_llik_0(int p, NumericVector par, void *ex);
-
 NumericVector f_pen_score_0(int p, NumericVector par, void *ex);
+NumericVector f_firth_score_0(int p, NumericVector par, void *ex);
 
 NumericMatrix f_ressco_0(int p, NumericVector par, void *ex);
 
 List logisregloop(int p, NumericVector par, void *ex,
-                  int maxiter, double eps, bool firth,
+                  int maxiter, double eps, bool firth, bool bc,
                   IntegerVector colfit, int ncolfit);
 
 double logisregplloop(int p, NumericVector par, void *ex,
-                      int maxiter, double eps, bool firth,
+                      int maxiter, double eps, bool firth, bool bc,
                       int k, int which, double l0);
 
 List logisregcpp(const DataFrame data,
@@ -44,8 +42,10 @@ List logisregcpp(const DataFrame data,
                  const std::string weight,
                  const std::string offset,
                  const std::string id,
+                 const std::string link,
                  const bool robust,
                  const bool firth,
+                 const bool bc,
                  const bool flic,
                  const bool plci,
                  const double alpha);
