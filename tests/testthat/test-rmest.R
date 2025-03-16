80f613b3674314311f27a635a96c21d41788c899
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:27:39 -0400

    add testthat:: prefix

--- tests/testthat/test-rmest.R
+++ tests/testthat/test-rmest.R
@@ -1,9 +1,9 @@
 library(dplyr, warn.conflicts = FALSE)
 
-test_that("rmest: valid milestone time works", {
-  df1 <- rmest(aml %>% filter(x == "Maintained"), time="time", 
+testthat::test_that("rmest: valid milestone time works", {
+  df1 <- rmest(aml %>% filter(x == "Maintained"), time="time",
                event="status", milestone=161)
-  
+
   # the following estimates can be obtained from SAS PROC LIFETEST
   #   proc lifetest data=aml rmst(tau=161);
   #     where x = "Maintained";
@@ -11,28 +11,28 @@ test_that("rmest: valid milestone time works", {
   #   run;
   rmst = 52.64545
   stderr = 19.8286
-  
-  expect_equal(round(df1$rmst, 5), rmst)
-  expect_equal(round(df1$stderr, 4), stderr)
+
+  testthat::expect_equal(round(df1$rmst, 5), rmst)
+  testthat::expect_equal(round(df1$stderr, 4), stderr)
 })
 
 
-test_that("rmest: bias correction for variance estimate", {
-  df1 <- rmest(aml %>% filter(x == "Maintained"), time="time", 
+testthat::test_that("rmest: bias correction for variance estimate", {
+  df1 <- rmest(aml %>% filter(x == "Maintained"), time="time",
                event="status", milestone=161, biascorrection=TRUE)
-  
+
   # the following estimates can be obtained from SAS PROC LIFETEST
   #   proc lifetest data=aml rmst(bc tau=161);
   #     where x = "Maintained";
   #     time time*status(0);
   #   run;
   stderr = 21.4173
-  
-  expect_equal(round(df1$stderr, 4), stderr)
+
+  testthat::expect_equal(round(df1$stderr, 4), stderr)
 })
 
 
-test_that("rmest: milestone should be <= largest observed time", {
-  expect_error(rmest(aml %>% filter(x == "Nonmaintained"), time="time", 
-               event="status", milestone=161))
+testthat::test_that("rmest: milestone should be <= largest observed time", {
+  testthat::expect_error(rmest(aml %>% filter(x == "Nonmaintained"),
+                               time="time", event="status", milestone=161))
 })
