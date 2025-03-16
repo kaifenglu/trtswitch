8c0e3a381aa1a7e81df6f8fc354de329cb936f77
Kaifeng Lu <kaifenglu@gmail.com>
2025-03-16 13:23:32 -0400

    add testthat:: prefix

--- tests/testthat/test-bscpp.R
+++ tests/testthat/test-bscpp.R
@@ -1,11 +1,15 @@
-test_that("bscpp: basis and attributes", {
+library(splines)
+
+testthat::test_that("bscpp: basis and attributes", {
   bs1 <- bscpp(women$weight, df = 5)
-  bs2 <- splines::bs(women$weight, df = 5)
-  expect_equal(`attributes<-`(bs1, NULL), `attributes<-`(bs2, NULL))
-  expect_equal(attr(bs1, "degree"), attr(bs2, "degree"))
-  expect_equal(attr(bs1, "knots"), attr(bs2, "knots"))
-  expect_equal(attr(bs1, "boundary_knots"), attr(bs2, "Boundary.knots"))
-  expect_equal(attr(bs1, "intercept"), attr(bs2, "intercept"))
+  bs2 <- bs(women$weight, df = 5)
+  testthat::expect_equal(`attributes<-`(bs1, NULL), 
+                         `attributes<-`(bs2, NULL))
+  testthat::expect_equal(attr(bs1, "degree"), attr(bs2, "degree"))
+  testthat::expect_equal(attr(bs1, "knots"), attr(bs2, "knots"))
+  testthat::expect_equal(attr(bs1, "boundary_knots"), 
+                         attr(bs2, "Boundary.knots"))
+  testthat::expect_equal(attr(bs1, "intercept"), attr(bs2, "intercept"))
 })
 
 
