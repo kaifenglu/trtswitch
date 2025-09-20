f2ad24375ecb3a5c327b80bbe2574d881263d068
Kaifeng Lu <kaifenglu@gmail.com>
2025-09-20 12:34:35 -0400

    changed the default value of boot from 1 to 0

--- src/ipcw.cpp
+++ src/ipcw.cpp
@@ -30,7 +30,7 @@ List ipcwcpp(
     const bool swtrt_control_only = 1,
     const double alpha = 0.05,
     const std::string ties = "efron",
-    const bool boot = 1,
+    const bool boot = 0,
     const int n_boot = 1000,
     const int seed = NA_INTEGER) {
   
